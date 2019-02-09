e4df6e81a0554ecd5fd9b6db6016653497c1cd7d
Florian Spieß <business@minnced.club>
2019-02-09 17:13:13 +0100

    Improve flux for pagination actions

--- src/main/kotlin/club/minnced/jda/reactor/extensions.kt
+++ src/main/kotlin/club/minnced/jda/reactor/extensions.kt
@@ -6,17 +6,28 @@ import net.dv8tion.jda.internal.requests.restaction.pagination.MessagePagination
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 
 fun <T> RestAction<T>.asMono() = Mono.fromFuture(this::submit)
 
 fun <T, M> PaginationAction<T, M>.asFlux(overflowStrategy: FluxSink.OverflowStrategy = FluxSink.OverflowStrategy.BUFFER): Flux<T>
     where M : PaginationAction<T, M> = Flux.create<T>({ sink ->
     cache(false)
-    forEachAsync {
-        sink.next(it)
-        true
-    }.toMono().doOnError(sink::error).doFinally { sink.complete() }
+
+    sink.onRequest { amount ->
+        if (amount <= 0) return@onRequest
+
+        var counter = amount
+        forEachAsync {
+            sink.next(it)
+            counter == Long.MAX_VALUE || --counter > 0
+        }.exceptionally {
+            sink.error(it)
+            null
+        }.thenRun {
+            if (counter > 0)
+                sink.complete()
+        }
+    }
 }, overflowStrategy)
 
 fun main() {
